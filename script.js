fe219a1bb6f2448bd338ca6672f33249a1121499
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-09-10 20:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,24 +3,19 @@ function Car(make, model) {
   this.model = model;
 }
 
-// Add method to Car prototype
 Car.prototype.getMakeModel = function() {
   return this.make + " " + this.model;
 };
 
 function SportsCar(make, model, topSpeed) {
-  // Call Car constructor to inherit properties
   Car.call(this, make, model);
   this.topSpeed = topSpeed;
 }
 
-// Inherit Car prototype
 SportsCar.prototype = Object.create(Car.prototype);
 
-// Reset constructor (important step after inheritance)
 SportsCar.prototype.constructor = SportsCar;
 
-// Add method to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function() {
   return this.topSpeed;
 };
