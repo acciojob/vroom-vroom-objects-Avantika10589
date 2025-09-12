27e6053cb1e6d6772267b5ef02172fceb536bebc
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-09-12 11:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-function Car(make, model) {
-  this.make = make;
-  this.model = model;
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  getMakeModel() {
+    return this.make + " " + this.model;
+  }
 }
 
-Car.prototype.getMakeModel = function() {
-  return this.make + " " + this.model;
-};
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make, model); 
+    this.topSpeed = topSpeed;
+  }
 
-function SportsCar(make, model, topSpeed) {
-  Car.call(this, make, model);
-  this.topSpeed = topSpeed;
+  getTopSpeed() {
+    return this.topSpeed;
+  }
 }
 
-SportsCar.prototype = Object.create(Car.prototype);
-
-SportsCar.prototype.constructor = SportsCar;
-
-SportsCar.prototype.getTopSpeed = function() {
-  return this.topSpeed;
-};
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 
-console.log(car.getMakeModel()); // Ferrari Testarossa
-console.log(car.getTopSpeed());  // 200
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());  
 
 // Do not change the code below
 window.Car = Car;
